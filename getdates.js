b375f576437c3be16316fde47d159a0a879fe4e1
Masilelaj69-hue <masilelaj69@gmail.com>
2026-07-04 19:46:55 +0200

    save changes

--- getdates.js
+++
@@ -1,5 +0,0 @@
-const yearSpan = document.getElementById("year");
-const currentYear = new Date().getFullYear();
-
-yearSpan.textContent = currentYear;.
-
--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,16 @@
+"use strict";
+
+/*
+  WDD 131 - Dynamic Web Fundamentals
+  This script updates the footer year dynamically
+*/
+
+// Get the span element where the year will go
 const yearSpan = document.getElementById("year");
+
+// Get current year
 const currentYear = new Date().getFullYear();
 
-yearSpan.textContent = currentYear;.
+// Insert year into HTML
+yearSpan.textContent = currentYear;
 
