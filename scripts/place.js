2516197a02e2bf332e2df46f960a36081ace4946
Masilelaj69-hue <masilelaj69@gmail.com>
2026-07-17 20:03:51 +0200

    country page completed

---
+++ scripts/place.js
@@ -0,0 +1,30 @@
+const temperature = 8;
+const windSpeed = 12;
+
+function calculateWindChill(temp, speed) {
+    return (
+        13.12 +
+        0.6215 * temp -
+        11.37 * Math.pow(speed, 0.16) +
+        0.3965 * temp * Math.pow(speed, 0.16)
+    ).toFixed(1);
+}
+
+const windChill = document.querySelector("#windchill");
+
+if (temperature <= 10 && windSpeed > 4.8) {
+
+    windChill.textContent =
+        `${calculateWindChill(temperature, windSpeed)} °C`;
+
+} else {
+
+    windChill.textContent = "N/A";
+
+}
+
+document.querySelector("#year").textContent =
+new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+`Last Modified: ${document.lastModified}`;
