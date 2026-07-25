d369275c3ce562a3faae18e6bd925190ceaf9fda
Masilelaj69-hue <masilelaj69@gmail.com>
2026-07-25 07:55:49 +0200

    filtered-temple Assignment completed

---
+++ scripts/filtered-temple.js
@@ -0,0 +1,30 @@
+const temples = [
+
+{
+templeName:"Aba Nigeria",
+location:"Aba, Nigeria",
+dedicated:"2005, August, 7",
+area:11500,
+imageUrl:"https://assets.churchofjesuschrist.org/..."
+},
+
+{
+templeName:"Manti Utah",
+location:"Manti, Utah",
+dedicated:"1888, May, 21",
+area:74792,
+imageUrl:"https://assets.churchofjesuschrist.org/..."
+},
+
+{
+templeName:"Salt Lake",
+location:"Salt Lake City, Utah",
+dedicated:"1893, April, 6",
+area:253000,
+imageUrl:"https://assets.churchofjesuschrist.org/..."
+}
+
+/* Add the remaining temples supplied by your instructor */
+/* Add at least THREE more yourself */
+
+];
